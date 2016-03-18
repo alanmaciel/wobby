afdb4d5acaac29bc030b54e83612af146c310a68
Alan Maciel <alan.maciel.salcedo@gmail.com>
2016-03-18 10:47:16 -0600

    Testing sending params

--- web/controllers/about_controller.ex
+++ web/controllers/about_controller.ex
@@ -1,8 +1,8 @@
 defmodule Wobby.AboutController do
   use Wobby.Web, :controller
 
-  def about_us(conn, _params) do
-    render conn, "about_us.html"
+  def about_us(conn, %{"name" => name}) do
+    render conn, "about_us.html", name: name
   end
 
 end
